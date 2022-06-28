2e8d4e4fc49d1a777a4a17bcf9e0b00db66c926b
biscatcho <karimbiscatcho@gmail.com>
2022-06-28 14:06:58 +0200

    check edits

--- resources/views/frontend/projects.blade.php
+++ resources/views/frontend/projects.blade.php
@@ -90,17 +90,7 @@
 
                         </div>
                         <div class="Pagination">
-                            {{-- <i class="fas fa-angle-left"></i>
-                            <ul>
-                                <li class="active">1</li>
-                                <li>2</li>
-                                <li>3</li>
-                                <li>4</li>
-                            </ul>
-                            <i class="fas fa-angle-right"></i> --}}
-
                             {{ $projects->links() }}
-
                         </div>
                     </div>
                 </div>
